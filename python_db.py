e0c245fcdb6ab25168a19f3e9ce52178633b8cc6
soniSUHANI <soni03suhani@gmail.com>
2023-01-08 22:13:06 +0530

    week 4 activity

---
+++ python_db.py
@@ -0,0 +1,9 @@
+import pdb
+
+
+pdb.set_trace()
+name = input('please type your name : ')
+age = input('please type your age : ')
+print(f'hello {name} your age is {age}')
+age2 = int(age) + 5
+print(f'{name} you will be {age2} in next five years')
